2b9e111537356977b5246d972be80cbbae84d673
Praveenaa <praveenaa2408@gmail.com>
2024-09-27 10:25:08 -0600

    removed footnote

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Map from './Map';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Wildfire Webapp</h1>
-      </header>
+
       <main>
         <Map />
       </main>
-      <footer>
-        <p>© 2024 Wildfire Webapp</p>
-      </footer>
+
     </div>
   );
 };
